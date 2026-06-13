bed93ca17b4a6455f197dd02524388ac6a47c1c6
rkan0 <65864071+rkan0@users.noreply.github.com>
2026-06-13 21:46:52 +0200

    completed js 6+7

--- javascript-exercises-main-detached/foundations/07_reverseString/reverseString.js
+++ javascript-exercises-main-detached/foundations/07_reverseString/reverseString.js
@@ -1,6 +1,15 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let array = string.split("")
+    console.log("array="+array)
+    let output = ""
+    for (i=(array.length-1); i>=0; i--) {
+        let char = array[i]
+        console.log(char)
+        output = output.concat(array[i])
+    }
+    return output
 };
+
 
 // Do not edit below this line
 module.exports = reverseString;
