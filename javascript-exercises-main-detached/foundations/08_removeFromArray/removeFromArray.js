e0d9d15087c26a160fbb37e4093fc9dbfe95bbac
rkan0 <65864071+rkan0@users.noreply.github.com>
2026-06-14 22:30:19 +0200

    completed js 8+9

--- javascript-exercises-main-detached/foundations/08_removeFromArray/removeFromArray.js
+++ javascript-exercises-main-detached/foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...number) {
+    let output = []
+    // for each element of array, pass it as 'check' to function
+    output = array.filter(function(check) {
+        // if function shows that check is not in number, filter 'in' back to output
+        return !number.includes(check) 
+    }) 
+
+
+    return output
 };
 
 // Do not edit below this line
--- javascript-exercises-main-detached/foundations/09_sumAll/sumAll.js
+++ javascript-exercises-main-detached/foundations/09_sumAll/sumAll.js
@@ -1,6 +1,24 @@
-const sumAll = function() {
+const sumAll = function(one, two) {
+    if (!Number.isInteger(one) || !Number.isInteger(two) || one < 0 || two < 0) {
+        return "ERROR"
+    } else {
+    input = [one, two]
+    input.sort((x, y) => x-y)// order in ascending order
+    let array = []
+    for (let i = input[0]; i <= input[1]; i++) {// create array: starting from one, until two, i++, 
+        array.push(i)// concat i+1
 
+    } 
+    sum = array.reduce(sumfunc)
+    console.log(array)
+    return sum
+    }
 };
+
+function sumfunc(total, num) {
+    return total + num
+}
+
 
 // Do not edit below this line
 module.exports = sumAll;
